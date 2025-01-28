6b04d6af76382a72d8d115b5adf121ad58035b61
SkyFutu <119418823+SkyFutu@users.noreply.github.com>
2025-01-29 02:54:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,49 +1,39 @@
-from pkg.plugin.context import register, handler, llm_func, BasePlugin, APIHost, EventContext
-from pkg.plugin.events import *  # 导入事件类
-
+from pkg.plugin.context import register, handler, BasePlugin, APIHost, EventContext
+from pkg.plugin.events import * 
+import requests
+import logging
+import mirai
 
 # 注册插件
-@register(name="Hello", description="hello world", version="0.1", author="RockChinQ")
-class MyPlugin(BasePlugin):
+@register(name="setu", description="setu plugin", version="0.1", author="SkyFuture")
+class PixivImagePlugin(BasePlugin):
 
     # 插件加载时触发
     def __init__(self, host: APIHost):
-        pass
+        self.logger = logging.getLogger(__name__)
 
     # 异步初始化
     async def initialize(self):
         pass
 
-    # 当收到个人消息时触发
+    def setu_url(self, url):
+        resp = requests.get(
+            url
+        )
+        return resp.url
+
+    # 当收到个人/群聊消息时触发
     @handler(PersonNormalMessageReceived)
-    async def person_normal_message_received(self, ctx: EventContext):
-        msg = ctx.event.text_message  # 这里的 event 即为 PersonNormalMessageReceived 的对象
-        if msg == "hello":  # 如果消息为hello
-
-            # 输出调试信息
-            self.ap.logger.debug("hello, {}".format(ctx.event.sender_id))
-
-            # 回复消息 "hello, <发送者id>!"
-            ctx.add_return("reply", ["hello, {}!".format(ctx.event.sender_id)])
-
-            # 阻止该事件默认行为（向接口获取回复）
-            ctx.prevent_default()
-
-    # 当收到群消息时触发
     @handler(GroupNormalMessageReceived)
-    async def group_normal_message_received(self, ctx: EventContext):
-        msg = ctx.event.text_message  # 这里的 event 即为 GroupNormalMessageReceived 的对象
-        if msg == "hello":  # 如果消息为hello
-
-            # 输出调试信息
-            self.ap.logger.debug("hello, {}".format(ctx.event.sender_id))
-
-            # 回复消息 "hello, everyone!"
-            ctx.add_return("reply", ["hello, everyone!"])
-
-            # 阻止该事件默认行为（向接口获取回复）
+    async def person_normal_message_received(self, ctx: EventContext):
+        msg = ctx.event.text_message.strip()  
+        if msg == "涩图":
+            image_url = "https://www.dmoe.cc/random.php"
+            image_msg = mirai.Image(url=self.setu_url(image_url))
+            ctx.add_return("reply", [image_msg, f"awa~"])
             ctx.prevent_default()
-
+            return
+        
     # 插件卸载时触发
     def __del__(self):
         pass
